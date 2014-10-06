057faa722a53327c4351f582fdf7bd79a97bf9b0
Xantomen <xantomen@gmail.com>
2014-10-06 16:30:11 +0200

    Updates to receiving and creating messages

--- insuranceimapp/viewsfunctions/user_main_menu_module.py
+++ insuranceimapp/viewsfunctions/user_main_menu_module.py
@@ -28,16 +28,37 @@ def user_main_menu(request):
 
     except:
         action_to_execute = ""
+        
+    try:
+        messages_to_clear = request.POST['messages_to_clear']
+
+    except:
+        messages_to_clear = ""
        
     
     group_profile = user_profile.getOrAddGroupProfile()
     messages_list = user_profile.getUnreadMessages()
     
+    messages_pks_to_clear_list = messages_to_clear.split(",")
+    
     if action_to_execute == "add_sale_success":
+        return HttpResponseRedirect(reverse('insuranceimapp:add_sale_success', args=()))
+    if action_to_execute == "clear_all_messages":
         return HttpResponseRedirect(reverse('insuranceimapp:add_sale_success', args=()))
     if action_to_execute == "logout":
         return HttpResponseRedirect(reverse('insuranceimapp:app_login', args=()))
 
+
+    #Clear Messages when something in the messages_to_clear list
+    if len(messages_pks_to_clear_list):
+        for pk_value in messages_pks_to_clear_list:
+            if pk_value:
+                try:
+                    catch_message = Message.objects.get(pk = int(pk_value))
+                    user_profile.removeUnreadMessage(catch_message)
+                    user_profile.addReadMessage(catch_message)
+                except:
+                    print "Message PK doesn't match"
        
     context = {"group_profile":group_profile,"messages_list":messages_list}
     return render(request,'insuranceimapp/user_main_menu.html',context)
